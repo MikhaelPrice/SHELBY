d803ab98d9831f1e25cb5c1fe39763f9bda7631d
MichaelPrice <mikhail.a.prus@gmail.com>
2022-08-29 23:37:41 +0300

    Added ADMIN function

--- src/main/java/com/SHELBY/domain/Role.java
+++ src/main/java/com/SHELBY/domain/Role.java
@@ -3,7 +3,7 @@ package com.SHELBY.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER, ADMIN;
 
     @Override
     public String getAuthority() {
