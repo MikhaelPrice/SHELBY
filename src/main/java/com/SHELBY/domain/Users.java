3bdc6db0fb6c0c886f3a6733d97f91d620a5d250
MichaelPrice <mikhail.a.prus@gmail.com>
2022-09-01 08:23:23 +0300

    Added style of interface

--- src/main/java/com/SHELBY/domain/Users.java
+++ src/main/java/com/SHELBY/domain/Users.java
@@ -22,6 +22,10 @@ public class Users implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+    public boolean isAdmin(){
+        return roles.contains(Role.ADMIN);
+    }
+
     public Long getId() {
         return id;
     }
